c35afdcd078deecfe3647d7179eca1312adbf9b7
Efim Kozhevnikov <tczdev@gmail.com>
2024-02-03 08:56:41 +0400

    Added dirt to generation

--- packs/core_world.js
+++ packs/core_world.js
@@ -15,8 +15,123 @@ class GenerateRules {
   }
 }
 
-// Common
+
+/**
+ * 
+ * 
+ * DIRT
+ * 
+ * 
+ */
+// Full Dirt
 GenerateRules.addRule(500, [
+    ["core", 0, 9],
+    ["core", 1, 9],
+    ["core", 2, 9],
+    ["core", 3, 9],
+    ["core", 4, 9],
+    ["core", 5, 9]
+], {
+    left: ["dirt", "dirt-to-common"],
+    right: ["dirt", "dirt-to-common"],
+    bottom: ["dirt", "dirt-to-common"],
+    top: ["dirt", "dirt-to-common"]
+});
+
+
+// TRANSITION
+
+// TOP-LEFT
+GenerateRules.addRule(1, [
+    ["core", 6, 9, "0:0"], ["core", 7, 9, "1:0"],
+    ["core", 6, 10, "0:1"], ["core", 0, 9, "1:1"],
+], {
+    left: ["common"],
+    right: ["dirt-to-common"],
+    bottom: ["dirt-to-common"],
+    top: ["common"]
+});
+// TOP-MID
+GenerateRules.addRule(1, [
+    ["core", 7, 9, "0:0"], ["core", 7, 9, "1:0"],
+    ["core", 0, 9, "0:1"], ["core", 0, 9, "1:1"],
+], {
+    left: ["dirt-to-common"],
+    right: ["dirt-to-common"],
+    bottom: ["dirt"],
+    top: ["common"]
+});
+// TOP-RIGHT
+GenerateRules.addRule(1, [
+    ["core", 7, 9, "0:0"], ["core", 8, 9, "1:0"],
+    ["core", 0, 9, "0:1"], ["core", 8, 10, "1:1"],
+], {
+    left: ["dirt-to-common"],
+    right: ["common"],
+    bottom: ["dirt-to-common"],
+    top: ["common"]
+});
+// MIDDLE-LEFT
+GenerateRules.addRule(1, [
+    ["core", 6, 10, "0:0"], ["core", 0, 9, "1:0"],
+    ["core", 6, 10, "0:1"], ["core", 0, 9, "1:1"],
+], {
+    left: ["common"],
+    right: ["dirt"],
+    bottom: ["dirt-to-common"],
+    top: ["dirt-to-common"]
+});
+// MIDDLE-RIGHT
+GenerateRules.addRule(1, [
+    ["core", 0, 9, "0:0"], ["core", 8, 10, "1:0"],
+    ["core", 0, 9, "0:1"], ["core", 8, 10, "1:1"],
+], {
+    left: ["dirt"],
+    right: ["common"],
+    bottom: ["dirt-to-common"],
+    top: ["dirt-to-common"]
+});
+// BOTTOM-LEFT
+GenerateRules.addRule(1, [
+    ["core", 6, 10, "0:0"], ["core", 0, 9, "1:0"],
+    ["core", 6, 11, "0:1"], ["core", 7, 11, "1:1"],
+], {
+    left: ["common"],
+    right: ["dirt-to-common"],
+    bottom: ["common"],
+    top: ["dirt-to-common"]
+});
+// BOTTOM-MID
+GenerateRules.addRule(1, [
+    ["core", 0, 9, "0:0"], ["core", 0, 9, "1:0"],
+    ["core", 7, 11, "0:1"], ["core", 7, 11, "1:1"],
+], {
+    left: ["dirt-to-common"],
+    right: ["dirt-to-common"],
+    bottom: ["common"],
+    top: ["dirt"]
+});
+// BOTTOM-RIGHT
+GenerateRules.addRule(1, [
+    ["core", 0, 9, "0:0"], ["core", 8, 10, "1:0"],
+    ["core", 7, 11, "0:1"], ["core", 8, 11, "1:1"],
+], {
+    left: ["dirt-to-common"],
+    right: ["common"],
+    bottom: ["common"],
+    top: ["dirt-to-common"]
+});
+
+/**
+ * 
+ * 
+ * COMMON
+ * 
+ * 
+ */
+
+// Common
+GenerateRules.addRule(600, [
   ["core", 0, 0],
   ["core", 0, 2],
   ["core", 0, 3],
@@ -33,7 +148,7 @@ GenerateRules.addRule(500, [
  */
 
 // Horizontal
-GenerateRules.addRule(7, [
+GenerateRules.addRule(15, [
   ["core", 0, 6, "0:0"], ["core", 0, 6, "1:0"],
   ["core", 0, 7, "0:1"], ["core", 0, 7, "1:1"],
 ], {
@@ -42,7 +157,7 @@ GenerateRules.addRule(7, [
 });
 
 // Vertical
-GenerateRules.addRule(7, [
+GenerateRules.addRule(15, [
   ["core", 1, 6, "0:0"], ["core", 2, 6, "1:0"],
   ["core", 1, 6, "0:1"], ["core", 2, 6, "1:1"],
 ], {
@@ -51,7 +166,7 @@ GenerateRules.addRule(7, [
 });
 
 // Crossroad
-GenerateRules.addRule(1, [
+GenerateRules.addRule(2, [
   ["core", 8, 5, "0:0"], ["core", 9, 5, "1:0"],
   ["core", 8, 6, "0:1"], ["core", 9, 6, "1:1"],
 ], {
